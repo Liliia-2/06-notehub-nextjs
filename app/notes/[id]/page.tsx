5f61c92f1d6981038891a01e31cc17b0fba43ce6
Liliia <plv-86@ukr.net>
2026-05-26 22:49:01 +0300

    third commit

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -9,27 +9,26 @@ import { fetchNoteById } from '@/lib/api';
 import NoteDetailsClient from './NoteDetails.client';
 
 interface NoteDetailsPageProps {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 }
 
 export default async function NoteDetailsPage({
   params,
 }: NoteDetailsPageProps) {
-  const queryClient = new QueryClient();
+  const {id} = await params;
 
-  await queryClient.prefetchQuery({
-    queryKey: ['note', params.id],
-    queryFn: () =>
-      fetchNoteById(params.id),
-  });
+  
 
-  return (
-    <HydrationBoundary
-      state={dehydrate(queryClient)}
-    >
-      <NoteDetailsClient />
-    </HydrationBoundary>
+    return (
+       <div>
+      Note ID: {id}
+    </div>
+    // <HydrationBoundary
+    //   state={dehydrate(queryClient)}
+    // >
+    //   <NoteDetailsClient />
+    // </HydrationBoundary>
   );
 }
